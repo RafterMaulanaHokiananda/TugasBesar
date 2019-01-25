6c2824f48eb3101345903dd548ea484fdaec7965
RafterMaulanaHokiananda <42019738+RafterMaulanaHokiananda@users.noreply.github.com>
2019-01-25 15:08:46 +0700

    Add files via upload

---
+++ timeline.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PEMILU</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=Aleo" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <div>
+            <ul>
+                <li><a class="login" href="">Login</a></li>
+                <li><a class="register" href="register.php">Register</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <section class="box">
+        <div>
+            
+
+</body>
+</html>
